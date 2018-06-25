75da7a24bcc147d10bb331c60ce7861f46a65a21
MustaphaBoukhari <boukhari.mustapha31@gmail.com>
2018-06-25 14:44:24 +0200

    v0.01

---
+++ src/com/m2i/poe/Main.java
@@ -0,0 +1,43 @@
+package com.m2i.poe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int[] tab = {2, -2, 99, 88, 7, 13, 12, 10, 18, 22};
+        System.out.println(isPrime(7));
+        System.out.println(isPrime(6));
+        System.out.println(isPrime(1001));
+        int[] result = getPrimeNumbers(tab);
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        boolean result = true;
+        if (n < 2) {
+            result = false;
+        } else {
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static int[] getPrimeNumbers(int[] tab) {
+        int[] result = new int[tab.length];
+        int index = 0;
+        for (int i : tab) {
+            if (isPrime(i)) {
+                result[index] = i;
+                index++;
+            }
+        }
+        return result;
+    }
+}
